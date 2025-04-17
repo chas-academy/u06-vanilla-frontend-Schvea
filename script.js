7d4cccc125916aad96244fe1af5a8473c0d23f21
scvhea <svea.holm03@gmail.com>
2025-04-17 14:39:29 +0200

    hämta o rendera filmer + radera knappt

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 const apiUrl = "https://restfulapi-1.onrender.com/api/movies"; 
 
 class MovieApp {
-    
+    constructor(apiUrl, listElementId) {
+        this.apiUrl = apiUrl;
+        this.moviesList = document.getElementById(listElementId);
+    }
+
+    async getAllMovies() {
+        try {
+            const response = await fetch(this.apiUrl);
+            const data = await response.json();
+            if (Array.isArray(data)) {
+                this.renderMovies(data);
+            } else {
+                console.error("Är ej array:", data);
+            }
+        } catch (error) {
+            console.error("Error fetching movies:", error);
+        }
+    }
+    renderMovies(movies) {
+        this.moviesList.innerHTML = '';
+
+        movies.forEach(movie => {
+            const movieElement = document.createElement('div');
+            movieElement.classList.add('movie');
+
+            movieElement.innerHTML = `
+            <button onclick="app.deleteMovie('${movie._id}')">Radera</button>
+        `;
+            this.moviesList.appendChild(movieElement);
+        });
+    }
+
 }
+const app = new MovieApp(apiUrl, "movies-list");
+app.getAllMovies();
